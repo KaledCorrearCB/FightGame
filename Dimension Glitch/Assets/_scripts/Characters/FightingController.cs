6821440ae5d40a527a15dca50f660448cd0aa68a
KaledCorrearCB <56941264+KaledCorrearCB@users.noreply.github.com>
2025-11-09 16:58:39 -0500

    InPersonajer
    inPersonajes

---
+++ Dimension Glitch/Assets/_scripts/Characters/FightingController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FightingController : MonoBehaviour
+{
+
+    //movimiento
+    public float movementSpeed = 1f;
+    public float rotationSpeed = 10f;
+
+    void performovement()
+    {
+        float horizontarlInput = Input.GetAxis("Horizontal");
+
+    }
+}
