af7e52be5ece30404474f8d7de04ae51eb5a1c66
KaledCorrearCB <56941264+KaledCorrearCB@users.noreply.github.com>
2025-11-26 19:57:16 -0500

    ataque funcional casi que completo

--- Dimension Glitch/Assets/DamageBehaiuvor.cs
+++ Dimension Glitch/Assets/DamageBehaiuvor.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class DamageBehaiuvor : StateMachineBehaviour
 {
+    private PlayerAttack playerAta;
+    private FightingController figthc;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        playerAta = animator.GetComponent<PlayerAttack>();        
+        figthc = animator.GetComponent<FightingController>();   
+    }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -17,10 +20,10 @@ public class DamageBehaiuvor : StateMachineBehaviour
     //}
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.ResetTrigger("Hit");
+    }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
